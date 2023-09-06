c75ab53643cf07a30bfeba386e92c6aa0bc2ef86
BarzDev <barz.develop@gmail.com>
2023-09-06 15:01:59 +0700

    test

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
       <Link className="text-white font-bold" href={"/"}>
-        Bar&lsque;z CRUD
+        Bar'z CRUD
       </Link>
       <Link className="bg-white p-2 font-bold" href={"/addTopic"}>
         Add New
